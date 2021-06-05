1a59794c7777d78b8d00ec88524ba4323f92630c
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2021-06-05 18:36:11 +0530

    Wrapping Up

--- app/src/main/java/com/emojidictionary/EmojiAdapter.kt
+++ app/src/main/java/com/emojidictionary/EmojiAdapter.kt
@@ -1,33 +1,40 @@
 package com.emojidictionary
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.recyclelayout.view.*
 
-class EmojiAdapter : RecyclerView.Adapter<EmojiAdapter.TextHolder>() {
+class EmojiAdapter(val emolist : ArrayList<String>) : RecyclerView.Adapter<EmojiAdapter.TextHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextHolder {
        return TextHolder(LayoutInflater.from(parent.context).inflate(R.layout.recyclelayout,parent,false))
     }
 
     override fun onBindViewHolder(holder: EmojiAdapter.TextHolder, position: Int) {
-        holder.bindEmoji("😉")
+        val emo = emolist[position]
+        holder.bindEmoji(emo)
     }
 
     override fun getItemCount(): Int {
-        return 10
+        return emolist.count()
     }
     class TextHolder(v: View): RecyclerView.ViewHolder(v), View.OnClickListener{
         var view : View = v
-        var emo : String = ""
+        var emoji: String = ""
+        init{
+            v.setOnClickListener(this)
+        }
         fun bindEmoji(emoji: String){
-            this.emo = emoji
-            view.textView.text = emo
-
+            this.emoji = emoji
+            view.textView.text = emoji
         }
         override fun onClick(v: View?) {
-            TODO("Not yet implemented")
+            val detailIntent = Intent(view.context, EmojiDetailActivity::class.java)
+            detailIntent.putExtra("emoji",emoji)
+            startActivity(view.context,detailIntent,null)
         }
 
     }
