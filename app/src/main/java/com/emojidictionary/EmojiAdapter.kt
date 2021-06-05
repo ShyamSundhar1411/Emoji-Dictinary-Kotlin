aa1209d15eb0a9ada892d15eb52c11184de4c610
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2021-06-05 11:04:12 +0530

    Emoji Adapter

--- app/src/main/java/com/emojidictionary/EmojiAdapter.kt
+++ app/src/main/java/com/emojidictionary/EmojiAdapter.kt
@@ -1,22 +1,31 @@
 package com.emojidictionary
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.recyclelayout.view.*
 
 class EmojiAdapter : RecyclerView.Adapter<EmojiAdapter.TextHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextHolder {
-        TODO("Not yet implemented")
+       return TextHolder(LayoutInflater.from(parent.context).inflate(R.layout.recyclelayout,parent,false))
     }
 
-    override fun onBindViewHolder(holder: TextHolder, position: Int) {
-        TODO("Not yet implemented")
+    override fun onBindViewHolder(holder: EmojiAdapter.TextHolder, position: Int) {
+        holder.bindEmoji("😉")
     }
 
     override fun getItemCount(): Int {
         return 10
     }
     class TextHolder(v: View): RecyclerView.ViewHolder(v), View.OnClickListener{
+        var view : View = v
+        var emo : String = ""
+        fun bindEmoji(emoji: String){
+            this.emo = emoji
+            view.textView.text = emo
+
+        }
         override fun onClick(v: View?) {
             TODO("Not yet implemented")
         }
